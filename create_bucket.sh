e938ba655e9a578cdb2cc2db49a79634b99dff9b
Sergei Afanasenko <afanasenko.s@gmail.com>
2021-11-19 14:01:32 +0300

    Initial commit

---
+++ create_bucket.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# !!! Set appropriate variables
+bucketname=sergiochoo-wordpress-tfstate
+region=eu-central-1
+## end of vars section
+
+# create bucker
+aws s3api create-bucket --bucket $bucketname --region $region \
+    --create-bucket-configuration LocationConstraint=$region
+
+# enable bucket versioning
+aws s3api put-bucket-versioning --bucket $bucketname --versioning-configuration Status=Enabled
+
+# enable bucket encryption
+aws s3api put-bucket-encryption \
+    --bucket $bucketname \
+    --server-side-encryption-configuration '{"Rules": [{"ApplyServerSideEncryptionByDefault": {"SSEAlgorithm": "AES256"}}]}'
