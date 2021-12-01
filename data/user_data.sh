5bfbbfab386a3dbd118e9b6c80192b9543b53c7c
Sergei Afanasenko <afanasenko.s@gmail.com>
2021-12-01 17:10:22 +0300

    Added some internal log

--- data/user_data.sh
+++ data/user_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Check how long it takes to setup
+date > /tmp/time.txt
+
 # Install AWS EFS Utilities
 sudo yum update -y
 sudo yum install -y amazon-efs-utils
@@ -18,3 +21,6 @@ sudo service docker enable
 sudo docker run --name wordpress -p 80:80 -e WORDPRESS_DB_HOST=${db_host}:3306 \
                 -e WORDPRESS_DB_USER=${db_username} -v /efs:/var/www/html -e WORDPRESS_DB_PASSWORD=${db_password} \
                 -e WORDPRESS_DB_NAME=${db_name} -d wordpress
+
+# Check how long it takes to setup
+date >> /tmp/time.txt
